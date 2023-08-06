099a150372428827ad1c2c1f83a7b69cf3953423
Sajan505 <141566261+Sajan505@users.noreply.github.com>
2023-08-06 19:58:21 +0530

    Create header.h

---
+++ header.h
@@ -0,0 +1,5 @@
+extern void lcd_string(char *ptr);
+extern void delay_sec(unsigned int);
+extern void lcd_init(void);
+extern void lcd_data(unsigned char data);
+extern void lcd_cmd(unsigned char cmd);
